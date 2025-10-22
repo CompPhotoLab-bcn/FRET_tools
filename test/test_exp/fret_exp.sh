16ee15d1eac19e0a42777830fb35aae1474a2a60
Carles Curutchet <carles.curutchet@ub.edu>
2025-10-22 19:45:43 +0200

    Updated naproxen test files

--- test/test_exp/fret_exp.sh
+++ test/test_exp/fret_exp.sh
@@ -1,2 +1,1 @@
-../../src/fret_exp.py naproxen_280nm.in naproxen_280nm.pdf --x_title "Emission wavelength (nm)" --y_title "Intensity" --plot_title "Naproxen (excitation at 280nm)" --width 7.0 --height 3.5 --wavelength 315 --show_plot True
-
+../../src/fret_exp.py  naproxen_295nm.in naproxen_295nm.pdf --x_title "Emission wavelength (nm)" --y_title "Intensity" --plot_title "naproxen (excitation at 295 nm)" --width 7.0 --height 3.5 --wavelength 315 --show_plot True
